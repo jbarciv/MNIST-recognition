d7c37573b8125c25ba816d7b81be028388381d23
David <davidredquin@gmail.com>
2024-04-17 18:26:18 +0200

    pca.m

--- pca.m
+++ pca.m
@@ -9,12 +9,15 @@
 %   - Josep Maria Barbera Civera (17048)
 % First version: 29/04/2024
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%
+clear all
+close all
+clc
 
 %%% Loading data %%%
 load Trainnumbers.mat;
 
-%% No Neural Network classification
+%% No Neural Network classification %%
 
 % Separate training set from test set
 
@@ -23,25 +26,50 @@ y_train = Trainnumbers.label(:,1:8000);
 X_test = Trainnumbers.image(:,8001:10000);
 y_test = Trainnumbers.label(:,8001:10000);
 
+%%% Normalizacion %%%
+[D,N]=size(X_train); 
+clear meanp stdp
+meanp=mean(X_train')';
+stdp=std(X_train')';
+
+for i=1:D
+    if stdp(i) == 0 
+            stdp(i)=0.0001;
+    end
+end
+%%% Datos Train normalizados:
+for i=1:N
+    value=(X_train(:,i)-meanp)./stdp;
+    X_train_normalized(:,i)=value;
+end
+[D,N]=size(X_test); 
+
+%%% Datos Test normalizados:
+for i=1:N
+    value=(X_test(:,i)-meanp)./stdp;
+    X_test_normalized(:,i)=value;
+end
+
 %%%% Clasification %%%%
 
-print_digit(Trainnumbers.image,10)
+print_digit(Trainnumbers.image,10);
+n_dim=100;
+[train,reconst1,W]=processing_data(X_train_normalized,n_dim,meanp,stdp);
+[test,reconst2]=processing_data_post(X_test_normalized,meanp,stdp,W);
 
-[train,reconst1] = processing_data(X_train,y_train,154);
-[test,reconst2] = processing_data(X_test,y_test,154);
+print_digit(reconst1,50)
+print_digit(reconst2,100)
 
-print_digit(reconst1,33)
-
-% test=processing_data(X_test,y_test,100);
-train = real(train);
-test = real(test);
-
-knnMdl = fitcknn(train,y_train','NumNeighbors',5);
-knnclass = predict(knnMdl,test);
-
-no_errors_nn = length(find(knnclass'~=y_test));
+knnMdl = fitcknn(train',y_train','NumNeighbors',5);
+knnclass = predict(knnMdl,test');
+no_errors_nn=length(find(knnclass'~=y_test));
 disp(['Misclassification error: ', num2str(no_errors_nn)]);
 disp(['Acierto: ', num2str((2000-no_errors_nn)/2000)]);
+
+cm = confusionchart(y_test,knnclass', ...
+    'Title','Matriz de confusión', ...
+    'RowSummary','row-normalized', ...
+    'ColumnSummary','column-normalized');
 
 %% Neural Network classification
 
@@ -49,118 +77,70 @@ disp(['Acierto: ', num2str((2000-no_errors_nn)/2000)]);
 
 
 %% Functions
-function [train, reconst] = processing_data(X_train, y_train, n_dim)
-    %%% Normalization
-    [D,N] = size(X_train); 
-    clear meanp stdp
-    meanp = mean(X_train')';
-    stdp = std(X_train')';
+function [train,reconst,W]=processing_data(X_train,n_dim,meanp,stdp)
 
-    %%% Normalized data
-    for i = 1:N
-        value = (X_train(:,i)-meanp)./stdp;
-        for j = 1:D
-            pixel = value(j);
-            if isnan(pixel)
-                value(j) = 0;
-            end
-        end
-        image_normalized(:,i) = value;
-    end
-    
-    %%% PCA %%%%%%%%%%%%%%%%%%
-    % image_normalized = X_train;
-    data = image_normalized;
-    original_data = X_train;
-    
+    data = X_train;   
+
     % Compute PCA transformation matrix using normalized training data
-    [Wc, Diag] = eig(cov(data'));
-    [D, N] = size(data); 
+    [Wc,Diag]=eig(cov(data'));
+    [D,N]=size(data); 
     total = sum(sum(Diag));
     eval = max(Diag);
 
-    explained_variance = 0.7;
-    for i = 0:783
-        test = sum(eval((784-i):784))/total;
-        if (test >= explained_variance)
-            k = i;
-            break
-        end
-    end
-    Vk = Wc(:,((784-(n_dim-1)):784));
-    k = n_dim;
-    train_tilde = data;
-    % F = train_tilde'*Vk;
-    F = Vk'*train_tilde;
-    pnproj = F;
-    compression_ratio = k/784;
-    train_tilde_k = Vk*F;
-    reconst = train_tilde_k;
-
-
-    % for i=1:n_dim
-    %     W(i,:)=Wc(:,D+1-i)'; 
+    % explained_variance = 0.7;
+    % for i = 0:783
+    %     test = sum(eval((784-i):784))/total;
+    %     if test >= explained_variance
+    %         k = i;
+    %         break
+    %     end
     % end
-    % pnproj=W*data;
-    % ExpectedError=0;
-    % Diag(D,D)
-    % for j=0:n_dim
-    %     ExpectedError=ExpectedError+Diag(D-j,D-j);
+    % Informacion_mantenida = sum(eval((784-n_dim):784))/total;
+    % Vk = Wc(:,((784-(n_dim-1)):784));
+    % k=n_dim;
+    % train_tilde=data;  
+    % F = Vk'*train_tilde;
+    % pnproj=F;
+    % compression_ratio = k/784;
+    % train_tilde_k = Vk*F;
+    % reconst = train_tilde_k;
+    % for i=1:N       
+    %     p_reconstructed(:,i)=train_tilde_k(:,i).*stdp+meanp;
     % end
-    % %error=Vk-W'
-    % % ExpectedError
-    % p_reconstructed=W'*pnproj;
-    % 
-    % p_reconstructed=p_reconstructed.*stdp+meanp;
-    % 
     % reconst=p_reconstructed;
-
-    %%% LDA %%%%%%%%%%%%%%%%%%
-    
-    % Step 1: Compute array Wnlda
-    % Assuming X_train is your training data and y_train is the corresponding labels
-    X_train = pnproj';
-    y_train = y_train';
-    class_labels = unique(y_train);
-    num_classes = numel(class_labels);
-    num_features = k;
-
-    % Calculate class mean
-    class_means = zeros(num_classes, num_features);
-    for i = 1:num_classes
-        class_means(i, :) = mean(X_train(y_train == class_labels(i), :));
+%%
+    for i=1:n_dim
+        W(i,:)=Wc(:,D+1-i)'; 
     end
+    pnproj=W*data;
+    ExpectedError=0;
+    Diag(D,D);
+    for j=0:n_dim
+        ExpectedError=ExpectedError+Diag(D-j,D-j);
+    end    
+    ExpectedError=ExpectedError/total
 
-    % Within-class scatter matrix
-    S_W = zeros(num_features, num_features);
-    for i = 1:num_classes
-        class_data = X_train(y_train == class_labels(i), :);
-        class_mean_centered = class_data - class_means(i, :);
-        S_W = S_W + (class_mean_centered' * class_mean_centered);
+    pnproj = W*data;
+    reconstructed=W'*pnproj;
+    for i=1:N       
+        p_reconstructed(:,i)=reconstructed(:,i).*stdp+meanp;
     end
-
-    % Between-class scatter matrix
-    overall_mean = mean(pnproj');
-    S_B = zeros(num_features, num_features);
-    for i = 1:num_classes
-        n = sum(y_train == class_labels(i));
-        mean_diff = class_means(i, :) - overall_mean;
-        S_B = S_B + n * (mean_diff' * mean_diff);
-    end
-
-    % Computing W (eigenvecs) matrix: maximizing the Fisher Discriminants
-    [eigvecs, eigvals] = eig(pinv(S_W) * S_B);
-
-    % Choose the top k eigenvectors
-    [~, idx] = sort(diag(eigvals), 'descend');
-    Wland = eigvecs(:, idx(1:k-1));
-
-    % Step 2: Compute 1D coordinates of test data
-    X_test_projected_10D = X_train*Wland;
-
-    train = X_test_projected_10D;
-    % train=pnproj;
+    reconst=p_reconstructed;
+    train=pnproj;
 end
+
+
+function [train,reconst]=processing_data_post(X_train,meanp,stdp,W)
+    data = X_train;  
+    [D,N]=size(data);   
+    pnproj = W*data;
+    reconstructed=W'*pnproj;
+    for i=1:N       
+        p_reconstructed(:,i)=reconstructed(:,i).*stdp+meanp;
+    end
+    reconst=p_reconstructed;
+    train=pnproj;
+end 
 
 function print_digit(digits,num)
     digit = zeros(28, 28); % Initialize the digit matrix
