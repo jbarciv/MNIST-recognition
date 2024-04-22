556b709145f3204f25a2a101c8584ab65dba5595
chema <chemabc@gmail.com>
2024-04-22 20:46:38 +0200

    LDA and plots

--- lda.m
+++ lda.m
@@ -1,140 +1,189 @@
-%% LDA for MNIST
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% LDA for MNIST %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all
 close all
 clc
 
-%%% Loading data %%%
+%% Loading data
 load Trainnumbers.mat;
 
-%% No Neural Network classification %%
-
-% Separate training set from test set
+%% Separate training set from test set
 X_train = Trainnumbers.image(:,1:8000);
 y_train = Trainnumbers.label(:,1:8000);
 X_test = Trainnumbers.image(:,8001:10000);
 y_test = Trainnumbers.label(:,8001:10000);
 
-%%% Normalization %%%
-[D, N] = size(X_train); 
-clear meanp stdp
-meanp = mean(X_train')';
-stdp = std(X_train')';
+%% Normalize Training Data
+[D_train, N_train] = size(X_train);
+[X_train_normalized, meanp, stdp] = normalize(X_train);
 
-for i = 1:D
-    if stdp(i) == 0 
-        stdp(i) = 0.0001;
-    end
-end
-
-%%% Training Data Normalized:
-for i = 1:N
-    value = (X_train(:, i) - meanp) ./stdp;
-    X_train_normalized(:, i) = value;
-end
-
-[D, N]=size(X_test); 
-
-%%% Test Data Normalized:
-for i = 1:N
+%% Normalize Test Data 
+[D_test, N_test] = size(X_test); 
+for i = 1:N_test
     value = (X_test(:, i) - meanp) ./ stdp;
     X_test_normalized(:,i) = value;
 end
 
-% print_digit(Trainnumbers.image,10);
-n_dim = 9;
+%% Visualize initial numbers with and without normalization
+% print_digit(X_train,10);
+% print_digit(X_train_normalized,10);
 
-%% Only LDA
-
-
-% Variables initialization
-dimension = size(X_train_normalized,1);
-Sw = zeros(dimension);
-Sb = zeros(dimension);   % Could consider sparse here
-N = size(X_train_normalized, 2); % Train data size
-Nt = size(y_train, 2); % Test data size
-Mu = mean(X_train_normalized, 2);  % Get mean vector of train data
-scrsz = get(groot, 'ScreenSize'); % Get screen width and height
-
+%% LDA for 2 and 3 dimensions - plotting
+n_dim = 2;
+%%% Choose between normalized data and raw data
+train_data = X_train_normalized; 
+%%% Variables initialization
+Sw = zeros(D_train);
+Sb = zeros(D_train);
+mu = meanp;
+%%% Compute Sw and Sb matrices
 for i = 0:9
-
-    % LDA Step 2. Construct Si matrix of each category
     mask = (y_train ==  i);
-    x = X_train_normalized(:, mask);
+    x = train_data(:, mask);
     ni = size(x, 2);
-    pi = ni / N;
+    pi = ni / N_train;
     mu_i = mean(x, 2);
-
     Si = (1/ni) * (x - repmat(mu_i, [1,ni]))*(x - repmat(mu_i, [1,ni]))';
-
-    % LDA Step 3. Construct Sw within class covariance
     Sw = Sw + Si * pi;
+    Sb = Sb + pi * (mu_i - mu) * (mu_i - mu)';
+end
+%%% Compute Singular Value Decomposition (SVD) of Sw\Sb
+M = pinv(Sw) * Sb;
+[U, D, V] = svd(M);
+%%% Compute projection matrix of n_dim
+W_2 = U(:, 1:n_dim);
+% Project the train data
+Y = W_2' * train_data;
+%%% Plot 2D and 3D projection for each number
+% subplot2D3Ddata(y_train, Y, n_dim)
+plot2D3Ddata(y_train, Y, n_dim)
+n_dim = 3;
+W_3 = U(:, 1:n_dim);
+Y = W_3' * train_data;
+% subplot2D3Ddata(y_train, Y, n_dim)
+plot2D3Ddata(y_train, Y, n_dim)
 
-    % LDA Step 4. Construct Sb between class covariance
-    Sb = Sb + pi * (mu_i - Mu) * (mu_i - Mu)';
+%% LDA for Classification
+
+% disp('LDA and kNN classification');
+% %%% Choose between normalized data and raw data
+% train_data = X_train;
+% test_data = Y_train;
+% for p = 1:9
+%     W = U(:, 1:p);
+% 
+%     Y = W' * train_data;
+%     Y_t = W' * T;
+% 
+%     % LDA Step 9 Classify test data using Nearest Neighbor
+%     accuracy = classifyNN(Y_t, Y, test_label, train_label);
+% 
+%     % Display Messages on the screen 
+%     message = ['Reduced dimension: ', num2str(p), ', ', ...
+%         'Classification accuracy: ', num2str(sum(accuracy)*100), '%, '];
+% 
+%     disp(message);
+% 
+%     % restore classification result in accuracy map
+%     iterator = iterator + 1;
+%     accuracy_mat(iterator, :) = accuracy;
+% end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Normalized a given data. Also returns mean and stdeviation
+function [normalized, meanp, stdp] = normalize(data)
+    [D, N] = size(data);
+    meanp = mean(data')';
+    stdp = std(data')';
+    for i = 1:D
+        if (stdp(i) == 0) 
+            stdp(i) = 0.0001;
+        end
+    end
+    for i = 1:N
+        value = (data(:, i) - meanp) ./stdp;
+        normalized(:, i) = value;
+    end
 end
 
-
-% LDA Step 5. Singular Value Decomposition of Sw\Sb
-M = pinv(Sw) * Sb;  % Sw maybe singular, use pseudo-inverse
-[U, D, V] = svd(M);
-
-% LDA Step 6 Reduce dimension to 2
-G2 = U(:, 1:n_dim);
-
-% LDA Step 7 Reconstruct the train data matrix
-Y2 = G2' * X_test_normalized;
-
-recons = G2 * Y2;
-
-print_digit(recons,10);
-
-
-% Plot 2d figure
-% data2d_fig = figure('Name', '2-D Plot');
-% set(data2d_fig, 'Position', [60 60 scrsz(3)-120 scrsz(4) - 140]);
-% for number = 0:9
-% 
-%     mask = (y_train ==  number);
-%     a = Y2(1,mask);
-%     b = Y2(2,mask);
-%     c = y_train(mask);
-% 
-%     % Draw 2D visualization in separate view
-%     subplot(2, 5, number+1)
-%     scatter(a',b');
-%     title(['Number ', num2str(number)]);
-% end
-
-
-% n_dim = 3; % Dimensionality
-% G3 = U(:, 1:n_dim);
-% Y3 = G3' * X_train_normalized;
-% 
-% % Plot 3d figure
-% data3d_fig = figure('Name', '3-D Plot');
-% set(data3d_fig, 'Position', [60 60 scrsz(3)-120 scrsz(4) - 140]);
-% for number = 0:9
-% 
-%     mask = (y_train ==  number);
-%     a = Y3(1, mask);
-%     b = Y3(2, mask);
-%     c = Y3(3, mask);
-%     subplot(2,5,number+1);
-%     scatter3(a',b',c');
-%     title(['Number ', num2str(number)]);
-% end
-
-
+% Print a digit from whole matrix of numbers
 function print_digit(digits,num)
-    digit = zeros(28, 28); % Initialize the digit matrix
-    n = 1;
-    % Extracting two different elements from each column
+    digit = zeros(28, 28);
     for i = 1:28
         for j = 1:28
             digit(i, j) = digits((i - 1) * 28 + j, num);
         end
     end
-    imshow(digit); % Display the digit
- end
+    figure;
+    imshow(digit);
+end
+
+%Plot 2D or 3D subplots in the same figure
+function subplot2D3Ddata(y_train, Y, dim)
+    scrsz = get(groot, 'ScreenSize');
+    if dim == 2
+        data_fig = figure('Name', '2-D Plot');
+    elseif dim == 3
+        data_fig = figure('Name', '3-D Plot');
+    else
+        error("only 2D and 3D dimensions are allowed!");
+    end
+    set(data_fig, 'Position', [60 60 scrsz(3)-120 scrsz(4) - 140]);
+    for number = 0:9
+        mask = (y_train ==  number);
+        a = Y(1, mask);
+        b = Y(2, mask);
+        if dim == 2
+            subplot(2, 5, number+1)
+            scatter(a', b');
+        elseif dim == 3
+            c = Y(3, mask);
+            subplot(2,5,number+1);
+            scatter3(a', b', c');
+        end
+        axis([-0.6 0.6 -10 14])
+        title(['Number ', num2str(number)]);
+    end
+end
+
+% Plot 2D or 3D in only one figure
+function plot2D3Ddata(y_train, Y, dim)
+    scrsz = get(groot, 'ScreenSize'); 
+    if dim == 2
+        data_fig = figure('Name', '2-D Plot');
+    elseif dim == 3
+        data_fig = figure('Name', '3-D Plot');
+    else
+        error("only 2D and 3D dimensions are allowed!");
+    end
+    set(data_fig, 'Position', [60 60 scrsz(3)-120 scrsz(4) - 140]);
+
+    colors = [0 0.4470 0.7410; 0.8500 0.3250 0.0980; 0.9290 0.6940 0.1250;...
+              0.4940 0.1840 0.5560; 0.4660 0.6740 0.1880; 0.3010 0.7450 0.9330;...
+              0.6350 0.0780 0.1840; 0 0.5 0; 1 0.4 0.6; 0.5 0.2 0.8];
+    hold on;
+    for number = 0:9
+        mask = (y_train ==  number);
+        a = Y(1, mask);
+        b = Y(2, mask);
+        if dim == 2
+            scatter(a, b, [], colors(number+1,:), 'filled', 'MarkerFaceAlpha', 0.5, 'DisplayName', ['Number ', num2str(number)]);
+        elseif dim == 3
+            c = Y(3, mask);
+            scatter3(a, b, c, [], colors(number+1,:), 'filled', 'MarkerFaceAlpha', 0.5, 'DisplayName', ['Number ', num2str(number)]);
+        end
+    end
+    hold off;
+
+    axis([-0.6 0.6 -10 14])
+    if dim == 2
+        legend('Location', 'best');
+    elseif dim == 3
+        legend('Location', 'bestoutside');
+    end
+end
 
--- pca.m
+++ pca.m
@@ -79,12 +79,11 @@ cm = confusionchart(y_test, knnclass', ...
     'RowSummary','row-normalized', ...
     'ColumnSummary','column-normalized');
 
-
-%%%% LDA %%%%
-
+%% LDA Reduction
 
 
-%% Neural Network classification
+
+%% Neural Network classification %%
 % to do
 
 %% Functions
@@ -166,46 +165,46 @@ function print_digit(digits, num)
 
 %% Bayesian Classifier
 function [bayMdl_Prior,errors_bay_Prior] = bayesian_classifier_training(X_train, y_train)
-warning('off')
-
-% Digit Probability
-digits = unique(y_train);
-count_digits = zeros(1,length(digits));
-prob_digits  = zeros(1,length(digits));
-for i = 1:length(digits)
-    count_digits(i) = length(find(y_train == digits(i)));
-    prob_digits(i)  = length(find(y_train == digits(i)))/length(y_train);
-end
-prior = prob_digits;
-
-% % Digit Mean
-% if length(X_train(:,1)) == 784
-%     digit_mean_image = zeros(length(X_train(:,1)),length(digits));
-%     for ii = 1:length(digits)
-%         digit_mean_image(:,ii) = round(mean(X_train(:,find(y_train==digits(ii))),2));
-%         figure();
-%         image(reshape(digit_mean_image(:,ii),28,28)');
-%     end
-% end
-
-% Include elements different from 0
-[r_train,c_train] = size(X_train);
-standard_deviation = 0.0001;
-X_train = X_train + standard_deviation.*randn(r_train,c_train);
-
-% Distribution "normal" - (Gaussian Distribution)
-% With Prior Probability
-bayMdl_Prior     = fitcnb(X_train',y_train','Prior',prior,'DistributionNames','normal');
-bayclass_Prior   = predict(bayMdl_Prior,X_train');
-errors_bay_Prior = length(find(bayclass_Prior'~=y_train));
-
-% Confusion Chart
-figure();
-cm = confusionchart(y_train',bayclass_Prior);
-cm.NormalizedValues;
-cm.RowSummary = 'row-normalized';
-cm.ColumnSummary = 'column-normalized';
-title('Bayesian Training - Confusion Matrix')
+    warning('off')
+    
+    % Digit Probability
+    digits = unique(y_train);
+    count_digits = zeros(1,length(digits));
+    prob_digits  = zeros(1,length(digits));
+    for i = 1:length(digits)
+        count_digits(i) = length(find(y_train == digits(i)));
+        prob_digits(i)  = length(find(y_train == digits(i)))/length(y_train);
+    end
+    prior = prob_digits;
+    
+    % % Digit Mean
+    % if length(X_train(:,1)) == 784
+    %     digit_mean_image = zeros(length(X_train(:,1)),length(digits));
+    %     for ii = 1:length(digits)
+    %         digit_mean_image(:,ii) = round(mean(X_train(:,find(y_train==digits(ii))),2));
+    %         figure();
+    %         image(reshape(digit_mean_image(:,ii),28,28)');
+    %     end
+    % end
+    
+    % Include elements different from 0
+    [r_train,c_train] = size(X_train);
+    standard_deviation = 0.0001;
+    X_train = X_train + standard_deviation.*randn(r_train,c_train);
+    
+    % Distribution "normal" - (Gaussian Distribution)
+    % With Prior Probability
+    bayMdl_Prior     = fitcnb(X_train',y_train','Prior',prior,'DistributionNames','normal');
+    bayclass_Prior   = predict(bayMdl_Prior,X_train');
+    errors_bay_Prior = length(find(bayclass_Prior'~=y_train));
+    
+    % Confusion Chart
+    figure();
+    cm = confusionchart(y_train',bayclass_Prior);
+    cm.NormalizedValues;
+    cm.RowSummary = 'row-normalized';
+    cm.ColumnSummary = 'column-normalized';
+    title('Bayesian Training - Confusion Matrix')
 end
 
 function [bayclass,errors_bay] = bayesian_classifier_testing(X_test,y_test,bayMdl)
